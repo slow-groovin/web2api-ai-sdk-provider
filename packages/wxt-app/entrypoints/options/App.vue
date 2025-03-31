58f88ffd6c971adc396f194ed481e64bb709036b
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2025-03-31 20:12:12 +0800

    init

---
+++ packages/wxt-app/entrypoints/options/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+import OpenAIComptable from './pages/OpenAIComptable.vue';
+</script>
+
+<template>
+  <OpenAIComptable />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #54bc4ae0);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ packages/wxt-app/entrypoints/options/pages/OpenAIComptable.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { generateText } from 'ai'
+import { createOpenAICompatible } from '@slow-groovin/openai-compatible';
+
+async function chat() {
+  const model = createOpenAICompatible({
+    name: 'siliconflow',
+    baseURL: 'https://api.siliconflow.cn/v1/',
+    apiKey: import.meta.env.WXT_SILICONFLOW_API_KEY,
+  })
+
+
+  const rs = await generateText({
+    model: model.languageModel('Qwen/Qwen2.5-7B-Instruct'),
+    prompt: 'give me a random sentence'
+  })
+  console.log('rs', rs)
+}
+</script>
+
+<template>
+  <div>
+    <h1>AI Debug</h1>
+    <button @click="chat">generateText</button>
+  </div>
+</template>
+
+<style scoped></style>
