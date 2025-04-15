8b1632f0bc7f3d39582be198607e8c608497fe78
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2025-04-15 20:21:46 +0800

    remove test-* packages in git

--- packages/test-custom-provider/src/index.ts
+++
@@ -1,2 +0,0 @@
-export const version = "0.0.1";
-export * from "./custom-provider-1";
--- packages/test-openai-comp/src/internal/index.ts
+++
@@ -1,4 +0,0 @@
-export { convertToOpenAICompatibleChatMessages } from '../convert-to-openai-compatible-chat-messages';
-export { mapOpenAICompatibleFinishReason } from '../map-openai-compatible-finish-reason';
-export { getResponseMetadata } from '../get-response-metadata';
-export type { OpenAICompatibleChatConfig } from '../openai-compatible-chat-language-model';
